64f77f0b19b6d81fd0a3c44c36e57c8b8716eb2f
Tomas <mallartomas@gmail.com>
2022-03-12 17:09:14 -0300

    second commit

--- scripts/desafio.js
+++ scripts/desafio.js
@@ -1,13 +1,14 @@
 function gen_table(){
     document.getElementById("tab").innerHTML="";
-    let n=Number(document.getElementById("capital").value);
-    let n2=Number(document.getElementById("couta").value);
-    let n3=Number(document.getElementById("interes").value);
-    if(n>0){   
-        for(i=1;i<=n2;i++){
-            ca=n/n2;
+    let capital=Number(document.getElementById("capital").value);
+    let couta=Number(document.getElementById("couta").value);
+    let interes=Number(document.getElementById("interes").value);
+    var arreglo = ["capital","cuota","interes"];
+    if(capital>0){   
+        for(i=1;i<=couta;i++){
+            ca=capital/couta;
             d1=ca.toFixed(2);
-            i2=((n*n3)/100)/n2;
+            i2=((capital*interes)/100)/couta;
             d2=i2.toFixed(2);
             r=ca+i2;
             d3=r.toFixed(2);
@@ -19,14 +20,15 @@ function gen_table(){
                         <td> ${d3}</td>
                     </tr>`;
         }
-        n1=n.toFixed(2);
-        t_i=i2*n2;
+        n1=capital.toFixed(2);
+        t_i=i2*couta;
         d4=t_i.toFixed(2);
-        t_p=r*n2;
+        t_p=r*couta;
         d5=t_p.toFixed(2);
         document.getElementById("t1").innerHTML=n1;
         document.getElementById("t2").innerHTML=d4;
-        document.getElementById("t3").innerHTML=d5;        
+        document.getElementById("t3").innerHTML=d5;  
+        console.log(arreglo);     
     }else{
         alert("Falta ingresar un Número");
     }
